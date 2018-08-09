02e899064fae3906cdf3ee46faef358aaf888182
Jakub Brehuv <jakub.brehuv@ibm.com>
2018-08-09 23:03:39 +0200

    boundary in movement

---
+++ SpaceShooter/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    public Boundary boundary;
+
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        GetComponent<Rigidbody>().velocity = movement * speed;
+
+        GetComponent<Rigidbody>().position = new Vector3(
+            Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+
+        );
+
+	}
+}
+ 
+
+
+[System.Serializable]
+public class Boundary{
+    public float xMin, xMax, zMin, zMax;
+
+}
