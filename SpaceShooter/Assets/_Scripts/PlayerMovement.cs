2e66b8f921a6753224a82ede14e1c8727f046cd5
Jakub Brehuv <kubek.breha@gmail.com>
2018-08-10 14:48:40 +0200

    ship movement and rotation

--- SpaceShooter/Assets/_Scripts/PlayerMovement.cs
+++ SpaceShooter/Assets/_Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public float speed;
+    public float tilt;
     public Boundary boundary;
 
 	void FixedUpdate () {
@@ -15,12 +16,14 @@ public class PlayerMovement : MonoBehaviour {
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         GetComponent<Rigidbody>().velocity = movement * speed;
 
-        GetComponent<Rigidbody>().position = new Vector3(
-            Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
-            0.0f,
-            Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+        //GetComponent<Rigidbody>().position = new Vector3(
+        //    Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+        //    0.0f,
+        //    Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
 
-        );
+        //);
+
+        GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, (GetComponent<Rigidbody>().velocity.x * -tilt));
 
 	}
 }
