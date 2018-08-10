6968647cab773e645ffa7ec25c7637c51caa6379
Jakub Brehuv <kubek.breha@gmail.com>
2018-08-10 16:13:09 +0200

    setup bold in editor

--- SpaceShooter/Assets/_Scripts/PlayerMovement.cs
+++ SpaceShooter/Assets/_Scripts/PlayerMovement.cs
@@ -16,12 +16,12 @@ public class PlayerMovement : MonoBehaviour {
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         GetComponent<Rigidbody>().velocity = movement * speed;
 
-        //GetComponent<Rigidbody>().position = new Vector3(
-        //    Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
-        //    0.0f,
-        //    Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+        GetComponent<Rigidbody>().position = new Vector3(
+            Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
 
-        //);
+        );
 
         GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, (GetComponent<Rigidbody>().velocity.x * -tilt));
 
