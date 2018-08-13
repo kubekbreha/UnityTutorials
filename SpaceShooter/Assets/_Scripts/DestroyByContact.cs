02c90d256b51d66ded808f901aeab80479211baf
Jakub Brehuv <kubek.breha@gmail.com>
2018-08-13 14:39:28 +0200

    explosion after contact

--- SpaceShooter/Assets/_Scripts/DestroyByContact.cs
+++ SpaceShooter/Assets/_Scripts/DestroyByContact.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
 
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+
     private void OnTriggerEnter(Collider other)
     {
+        Instantiate(explosion, transform.position, transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        }
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
